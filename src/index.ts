5a55c3c996ed1ccc35936526e81e526840a79ef8
Jannis Heydemann <heydemannjannis111@gmail.com>
2025-02-22 20:28:43 +0100

    linked index.html to script

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,15 @@ import { app, BrowserWindow } from 'electron';
 
 app.on('ready', () => {
     console.log('App is ready');
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    console.log('Window loaded');
+    win.loadFile('./build/index.html');
 });
 
-const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-        nodeIntegration: true
-    }
-});
 
-win.loadFile('index.html');
